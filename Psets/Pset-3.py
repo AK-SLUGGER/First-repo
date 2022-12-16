4e6cc32722ca93dba4ee12936bb95bd89d81e04b
AK <raoabhi001@gmail.com>
2022-12-16 20:59:47 +0530

    Psets

---
+++ Psets/Pset-3.py
@@ -0,0 +1,13 @@
+'''Captures USN of a student from std. input
+converts all lowercase letters to uppercase
+and print the same.
+'''
+
+#Read i/p from std.in
+#convert all lowercase to uppercase
+#Print the resulting string
+
+Name=input("Enter what you want in uppercase\n')
+Name = Name.upper()
+print(Name)
+           
