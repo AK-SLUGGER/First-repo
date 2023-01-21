c85c4a402dde8b5ce03556b3247c60aaa218151a
AK <raoabhi001@gmail.com>
2023-01-21 11:08:55 +0530

    Python notes

---
+++ Pyhton-notes/if_else-demo.py
@@ -0,0 +1,49 @@
+
+score = int(input("enter your marks\n"))
+
+if score>=35:
+   print("pass")
+if score<35:
+                  print("Fail")
+
+
+
+                   
+score = int(input("enter your marks\n"))
+
+if score>=35:
+   print("pass")
+#if score<35:
+   #print("Not pased")
+   
+else:
+    print("Not passed")
+    
+
+
+score = int(input("enter your marks\n"))
+
+if score>=70:
+   print("first class")
+if score>=60:
+    print("second class")
+if score>=35:    
+    print("pass")
+
+else:
+    print("Not passed")
+    
+
+score = int(input("enter your marks\n"))
+
+if score>=70:
+   print("first class")
+elif score>=60:#else block is the not
+    print("second class")
+elif score>=35:    
+    print("pass")
+
+else:
+    print("Not passed")
+    
+    
