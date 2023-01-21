197a0308656477d66b8e6cc686d26fca45fc3c24
AK-SLUGGER <120673978+AK-SLUGGER@users.noreply.github.com>
2023-01-21 14:30:01 +0530

    Revert "Main"

--- Pyhton-notes/Application of Infinite loop.py
+++
@@ -1,7 +0,0 @@
-
-
-x = float(input("Enter a Positive num\n"))
-while(x<=0):
-    x=float(input("Enter a Positive num\n"))
-
-print(f"the positive no u have entered is {x}")
--- Pyhton-notes/fuction 9-01-2022.py
+++
@@ -1,9 +0,0 @@
-
-
-def do_nothing():
-    """ Do nothing """
-
-print("before calling func(do_nothing)")
-do_nothing()
-print("after calling func(do_nothing)")
-    
