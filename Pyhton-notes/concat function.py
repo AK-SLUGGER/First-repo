cfb3e8b6aae3ef115afb09fd80fc461cd8aed2c0
AK-SLUGGER <120673978+AK-SLUGGER@users.noreply.github.com>
2023-02-09 12:34:28 +0530

    Delete Pyhton-notes directory

--- Pyhton-notes/concat function.py
+++
@@ -1,8 +0,0 @@
-#Concatenate word as many no times a user asks you
-'abcd'*2
-'''
-'''
-string_1='abcd'
-n_concat= input('how many times shall i concatenate\n')
-new_string=string_1*int(n_concat)
-print(new_string)
